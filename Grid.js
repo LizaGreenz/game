774b9a642833dd392672f5d840a778ac7bc7457e
LizaGreenz <gunenkoliza@gmail.com>
2022-04-28 00:10:21 +0300

    Added input

--- Grid.js
+++ Grid.js
@@ -79,6 +79,31 @@ class Cell {
     this.#tile.x = this.#x;
     this.#tile.y = this.#y;
   }
+
+  get mergeTile() {
+    return this.#mergeTile;
+  }
+
+  set mergeTile(value) {
+    this.#mergeTile = value;
+    if (value == null) return;
+    this.#mergeTile.x = this.#x;
+    this.#mergeTile.y = this.#y;
+  }
+
+  canAccept(tile) {
+    return (
+      this.tile == null ||
+      (this.mergeTile == null && this.tile.value === tile.value)
+    );
+  }
+
+  mergeTiles() {
+    if (this.tile == null || this.mergeTile == null) return;
+    this.tile.value = this.tile.value + this.mergeTile.value;
+    this.mergeTile.remove();
+    this.mergeTile = null;
+  }
 }
 
 function createCellElements(gridElement) {
--- Tile.js
+++ Tile.js
@@ -4,7 +4,7 @@ export default class Tile {
   #y;
   #value;
 
-  constructor(tileContainer, value = Math.random() > 0.9 ? 2 : 4) {
+  constructor(tileContainer, value = Math.random() > 0.5 ? 2 : 4) {
     this.#tileElement = document.createElement("div");
     this.#tileElement.classList.add("tile");
     tileContainer.append(this.#tileElement);
@@ -72,5 +72,21 @@ export default class Tile {
   set y(value) {
     this.#y = value;
     this.#tileElement.style.setProperty("--y", value);
+  }
+
+  remove() {
+    this.#tileElement.remove();
+  }
+
+  waitForTransition(animation = false) {
+    return new Promise((resolve) => {
+      this.#tileElement.addEventListener(
+        animation ? "animationend" : "transitionend",
+        resolve,
+        {
+          once: true,
+        }
+      );
+    });
   }
 }
